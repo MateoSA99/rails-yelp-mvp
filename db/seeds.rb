4ce9ae7cb5a3af17e16836facb26d1e2496cc214
mateo soto <mateo.sotoa@udea.edu.co>
2022-09-13 20:53:47 -0500

    Se crea Modelos y se hacen vistas

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts 'cleaning DB...'
+Restaurant.delete_all
+puts 'DB cleaned'
+
+10.times do
+  restaurant_instance = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_name,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: %w[chinese italian japanese french belgian].sample
+  )
+  5.times do
+    review = Review.new(
+      rating: rand(0..5),
+      content: Faker::Restaurant.review
+    )
+    review.restaurant = restaurant_instance
+    review.save
+  end
+  puts "Creating restaurant id: #{restaurant_instance.id}"
+end
+
+puts 'Finished'
+
