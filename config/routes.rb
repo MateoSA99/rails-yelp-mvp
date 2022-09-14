4148a647fe61e3c2377d5a9bf558fe3a51a98e99
mateo soto <mateo.sotoa@udea.edu.co>
2022-09-14 15:46:11 -0500

    Se hacen los nesting, y se crea el new y create de reviews, se dan estilos

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'reviews/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   root 'restaurants#index'
-  resources :restaurants
+  resources :restaurants do
+    resources :reviews, only: %i[new create]
+  end
 end
